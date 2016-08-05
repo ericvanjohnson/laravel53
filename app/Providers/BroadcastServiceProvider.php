f1f83088adbf8b11ffef5673768151255de682ed
Eric Van Johnson <eric@diegodev.com>
2016-08-05 13:37:02 -0700

    New Laravel 5.3 install

---
+++ app/Providers/BroadcastServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Broadcast;
+
+class BroadcastServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Broadcast::routes();
+
+        Broadcast::auth('example.*', function ($user, $exampleId) {
+            return true;
+        });
+    }
+}
