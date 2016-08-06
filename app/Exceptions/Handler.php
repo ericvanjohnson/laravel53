8dd45098ee0db7fc6cc90eee66d3bf249ca2dc45
Eric Van Johnson <eric@diegodev.com>
2016-08-06 02:02:37 -0700

    Echo

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -31,6 +31,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        \Log::error($exception);
         parent::report($exception);
     }
 
--- config/app.php
+++ config/app.php
@@ -176,6 +176,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Additional Service Providers
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
