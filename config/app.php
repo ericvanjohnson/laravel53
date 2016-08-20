94629adc990f75abbad134f3d599beccd87422cc
Eric Van Johnson <eric@diegodev.com>
2016-08-19 21:51:40 -0700

    Adding Scout and Updateing Laravel to RC1

--- config/app.php
+++ config/app.php
@@ -180,6 +180,7 @@ return [
          * Additional Service Providers
          */
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Laravel\Scout\ScoutServiceProvider::class,
 
     ],
 
